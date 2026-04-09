838a172ba61d828b64ce2ff653da016598cc6c35
muttyeb2003 <muttyeb2003@gmail.com>
2026-04-09 15:21:46 -0300

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'CoinPulse',
+  title: 'CryptoScope',
   description:
     'Crypto Screener App with built-in High-Frequency Terminal & Dashboard',
 };
